e1a4ff7234a4d49b848bd30cb61cd6b844308491
Holly Hatfield <h.hatfield@utexas.edu>
2013-04-23 18:55:58 -0500

    new tester files test.xml and fix.py

--- RunWCDB3.py
+++ RunWCDB3.py
@@ -3,16 +3,16 @@
 # -------
 
 import sys
-from WCDB3 import wcdb2_solve
+from WCDB3 import wcdb3_solve
 
 # ----
 # main
 # ----
 
-r = open('WCDB2.xml', 'r')
+r = open('WCDB3.xml', 'r')
 w = open('RunWCDB3.xml', 'w')
 
-wcdb2_solve(r,w)
+wcdb3_solve(r,w)
 
 r.close()
 w.close()
